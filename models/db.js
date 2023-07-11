854c5a36d7a703d412fdc8544d0988007d817e67
chimcha67 <chimchatitus@gmail.com>
2023-07-11 14:06:09 +0100

    update

--- models/db.js
+++ models/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const colors = require('colors')
 
+require('dotenv')
+
  const connectDb = async()=>{
     try {
-      const connect =  await mongoose.connect('mongodb://127.0.0.1:27017/License-reg-project', {
+      const connect =  await mongoose.connect(process.env.CONNECTION_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
