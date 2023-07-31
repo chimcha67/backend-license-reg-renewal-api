11ebcb2e8e47d93cfc28b4d7a34106a33c82020e
Kelenna <kellykowe12@gmail.com>
2023-07-31 14:36:51 +0100

    Update

--- user-routes/users.js
+++ user-routes/users.js
@@ -52,18 +52,7 @@ Router.delete('/delPrivateDoc/:id',validateToken, oldPrivateCarController.delete
 
 
 
-<<<<<<< HEAD:user-routs/users.js
 Router.post('/regNewCar',validateToken, uploader.array('image'), userService.newCar)
-=======
-Router.post('/regNewCar',validateToken, uploadFile.fields([
-    {name:'owner_passport'},
-    {name:'attestation_letter_image'},
-    {name:'purchase_receipt_image'},
-    {name:'delivery_note_image'},
-    {name:'proof_of_ownership_image'},
-    {name:'driver_license_image'}
-]), userService.newCar)
->>>>>>> Kelenna:user-routes/users.js
 Router.get('/getSingleNewCar/:id',validateToken, newCarController.getSingleCarCredentials)
 Router.put('/editNewCar/:id',validateToken,uploader.array('image'), userService.editNewCar)
 Router.delete('/delNewCar/:id',validateToken, newCarController.deleteCredential)
